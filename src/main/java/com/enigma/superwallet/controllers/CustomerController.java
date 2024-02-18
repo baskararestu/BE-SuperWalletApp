80a61451a61284aa8c40285ab88e7adef6db775a
baskara restu <baskararw10@gmail.com>
2024-02-18 15:28:10 +0700

    [FIX] customer controller for update

--- src/main/java/com/enigma/superwallet/controllers/CustomerController.java
+++ src/main/java/com/enigma/superwallet/controllers/CustomerController.java
@@ -11,6 +11,7 @@ import com.enigma.superwallet.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
